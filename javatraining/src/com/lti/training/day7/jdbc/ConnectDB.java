d19b6cb41c74055d0801097c17bc8d575ffc9835
vshadmin <vshadmin@Infva06203>
2019-06-08 13:29:38 -0700

    Lo

---
+++ javatraining/src/com/lti/training/day7/jdbc/ConnectDB.java
@@ -0,0 +1,33 @@
+package com.lti.training.day7.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+
+		public static void main(String[] args) {
+				Connection conn=null;
+				try {
+					//Loading the JDBC Driver
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+					//Establishing Connection with the database
+					String url = "jdbc:oracle:thin:@localhost:1521:XE";
+					String user = "hr";
+					String pass = "hr";
+					conn=DriverManager.getConnection(url,user,pass);
+					System.out.println("Connected successfully..");
+				}
+				catch(ClassNotFoundException e) {
+					System.out.println("JDBC driver not found");
+				}
+				
+				catch(SQLException e) {
+					e.printStackTrace();
+				}
+				
+				finally {
+					try { conn.close(); } catch(Exception e) { }
+				}
+		}
+}
